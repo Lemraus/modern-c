50ef6d8eda74ff4c8d5f789aaf56668a06814073
Armel Chausse <armel.chausse@student.ecp.fr>
2021-03-21 22:22:39 +0100

    [Challenge 1] Changing 'merge_adjacent' signature since one parameter was redundant

--- challenge-1/merge_sort/merge_sort.h
+++ challenge-1/merge_sort/merge_sort.h
@@ -1,5 +1,5 @@
 #ifndef MERGE_SORT
 #define MERGE_SORT
 void merge_sort(double ar[], size_t start, size_t end);
-void merge_adjacent(double ar[], size_t a_start, size_t a_end, size_t b_start, size_t b_end);
+void merge_adjacent(double ar[], size_t start, size_t middle, size_t end);
 #endif // MERGE_SORT
